c05d4e74e04a0d0d0c37174cff0b75e87e07f097
Hello <maths28@163.com>
2024-05-16 21:33:35 +0800

    将配置的变量独立到一个文件

--- 配置文件.py
+++ 配置文件.py
@@ -1,5 +1,7 @@
 import os
 import time
+import winreg
+
 
 进攻完毕次数 = 0
 循环最大等待秒数 = 30
@@ -19,3 +21,26 @@ import time
 部落冲突包名="com.tencent.tmgp.supercell.clashofclans"
 # 部落冲突包名=""com.supercell.clashofclans"#国际服
 # 升级判断最低金币=0
+
+# 注册表路径
+key = winreg.HKEY_CURRENT_USER
+sub_key = r"Software\leidian\LDPlayer9"
+value_name = "InstallDir"
+
+
+def get_registry_value(key, sub_key, value_name):
+    try:
+        # 打开注册表项
+        reg_key = winreg.OpenKey(key, sub_key)
+        # 读取注册表值
+        value, value_type = winreg.QueryValueEx(reg_key, value_name)
+        # 关闭注册表项
+        winreg.CloseKey(reg_key)
+        return value
+    except FileNotFoundError:
+        print("指定的注册表路径不存在")
+    except Exception as e:
+        print("发生错误:", e)
+
+# 获取注册表值
+雷电模拟器安装目录 = get_registry_value(key, sub_key, value_name)
